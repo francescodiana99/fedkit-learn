4e43d7c495e0f35a7c3fd55c4c30d1b776f4bfc8
francescodiana99 <francescodiana99@gmail.com>
2024-03-27 11:05:03 +0100

    minor fix evaluate_aia.py

--- scripts/evaluate_aia.py
+++ scripts/evaluate_aia.py
@@ -6,10 +6,6 @@ import pandas as pd
 
 import numpy as np
 
-import matplotlib.pyplot as plt
-
-import seaborn as sns
-
 from torch.utils.data import DataLoader
 
 from fedklearn.utils import get_param_tensor
