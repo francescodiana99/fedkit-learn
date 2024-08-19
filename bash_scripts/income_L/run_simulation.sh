4a8faabdad9ff3bc9a2536399bec6b9447dcf919
francescodiana99 <francescodiana99@gmail.com>
2024-08-20 01:45:01 +0200

    add bash script sim income l

---
+++ bash_scripts/income_L/run_simulation.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+cd ../../scripts
+$SCRIPT_DIR=../../scripts
+
+batch_size=32
+local_epochs=100
+learning_rate=5e-7
+device="cpu"
+task_name="income"
+heterogeneity=0.1
+num_rounds=100
+seed=42
+force_generation=False
+download_flag=False
+state="louisiana"
+optimizer="sgd"
+dp_flag=False
+task_name="income"
+
+
+# Function to display help message
+usage() {
+    echo "Usage: $0 --task_name --batch_size BATCH_SIZE --local_epochs LOCAL_EPOCHS --learning_rate LEARNING_RATE --device DEVICE \
+    --heterogeneity HETEROGENEITY --num_rounds NUM_ROUNDS --seed SEED --force_generation FORCE_GENERATION --download_flag DOWNLOAD_FLAG"
+    exit 1
+}
+
+while [ "$#" -gt 0 ]; do  # Use single brackets here
+    case $1 in
+
+        --num_rounds)
+            num_rounds="$2"
+            shift 2
+            ;;
+        --seed)
+            seed="$2"
+            shift 2
+            ;;
+        --force_generation)
+            force_generation="true"
+            shift 2
+            ;;
+        --download_flag)
+            download_flag="true"
+            shift 2
+            ;;
+        --task_name)
+            task_name="$2"
+            shift 2
+            ;;
+        --batch_size)
+            batch_size="$2"
+            shift 2
+            ;;
+        --local_epochs)
+            local_epochs="$2"
+            shift 2
+            ;;
+        --learning_rate)
+            learning_rate="$2"
+            shift 2
+            ;;
+        --device)
+            device="$2"
+            shift 2
+            ;;
+        --heterogeneity)
+            heterogeneity="$2"
+            shift 2
+            ;;
+        --help)
+            usage
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            usage
+            ;;
+    esac
+done
+
+cmd="python run_simulation.py --task_name income --test_frac 0.1 --scaler standard --optimizer sgd \
+--batch_size $batch_size --local_epochs $local_epochs --learning_rate $learning_rate --device $device \
+--by_epoch --local_steps $local_epochs --num_rounds $num_rounds --seed $seed \
+--data_dir ./data/income  --log_freq 5 --save_freq 1 \
+--model_config_path $SCRIPT_DIR/../fedklearn/configs/income/$state/models/net_config.json --split_criterion correlation \
+--n_tasks 10 --state $state --mixing_coefficient $heterogeneity \
+--chkpts_dir $SCRIPT_DIR/income/$state/heter/$heterogeneity/10/32/1/ \
+ --log_dir $SCRIPT_DIR/logs/income/$state/heter/$heterogeneity/10/32/1/ \
+ --metadata_dir $SCRIPT_DIR/metadata/income/$state/heter/$heterogeneity/10/32/1/"
+
+ if [ "$force_generation" = "true" ]; then
+    cmd="$cmd --force_generation"
+ fi
+
+if [ "$download_flag" = "true" ]; then
+        cmd="$cmd --download"
+fi
+
+echo "Running command: "
+echo $cmd
+eval $cmd
