8b0faaf0826663b302ca361ecda60db192b63773
francescodiana99 <francescodiana99@gmail.com>
2024-08-20 02:38:24 +0200

    add support Income-A

--- bash_scripts/income_L/run_simulation.sh
+++ bash_scripts/income_L/run_simulation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ../../scripts
-$SCRIPT_DIR='../../scripts'
+SCRIPT_DIR='../../scripts'
 
 batch_size=32
-local_epochs=100
+local_epochs=1
 learning_rate=5e-7
 device="cpu"
 task_name="income"
@@ -16,12 +16,14 @@ state="louisiana"
 optimizer="sgd"
 dp_flag=False
 task_name="income"
+split_criterion="correlation"
 
 
 # Function to display help message
 usage() {
-    echo "Usage: $0 --task_name --batch_size BATCH_SIZE --local_epochs LOCAL_EPOCHS --learning_rate LEARNING_RATE --device DEVICE \
-    --heterogeneity HETEROGENEITY --num_rounds NUM_ROUNDS --seed SEED --force_generation FORCE_GENERATION --download_flag DOWNLOAD_FLAG"
+    echo "Usage: $0  --batch_size BATCH_SIZE --local_epochs LOCAL_EPOCHS --learning_rate LEARNING_RATE --device DEVICE \
+    --heterogeneity HETEROGENEITY --num_rounds NUM_ROUNDS --seed SEED --force_generation FORCE_GENERATION --download_flag DOWNLOAD_FLAG \
+    --split_criterion SPLIT_CRITERION "
     exit 1
 }
 
@@ -38,15 +40,11 @@ while [ "$#" -gt 0 ]; do  # Use single brackets here
             ;;
         --force_generation)
             force_generation="true"
-            shift 2
+            shift 1
             ;;
         --download)
             download_flag="true"
-            shift 2
-            ;;
-        --task_name)
-            task_name="$2"
-            shift 2
+            shift 1
             ;;
         --batch_size)
             batch_size="$2"
@@ -68,6 +66,10 @@ while [ "$#" -gt 0 ]; do  # Use single brackets here
             heterogeneity="$2"
             shift 2
             ;;
+        --split_criterion)
+            split_criterion="$2"
+            shift 2
+            ;;
         --help)
             usage
             ;;
@@ -78,15 +80,42 @@ while [ "$#" -gt 0 ]; do  # Use single brackets here
     esac
 done
 
+if [ "$split_criterion" = "correlation" ]; then
+    state="louisiana"
+    dirs="--chkpts_dir ./$task_name/$state/heter/$heterogeneity/$batch_size/$local_epochs/ \
+ --logs_dir ./logs/$task_name/$state/heter/$heterogeneity/$batch_size/$local_epochs/ \
+ --metadata_dir ./metadata/$task_name/$state/heter/$heterogeneity/$batch_size/$local_epochs/ \
+  --mixing_coefficient $heterogeneity"
+then
+elif [ "$split_criterion" = "state" ]; then
+    state="full"
+    dirs="--chkpts_dir ./$task_name/$state/$batch_size/$local_epochs/ \
+    --logs_dir ./logs/$task_name/$state/$batch_size/$local_epochs/ \
+    --metadata_dir ./metadata/$task_name/$state/$batch_size/$local_epochs/"
+fi
+
+if [ "$state" != "full" ]; then
+    n_task=10
+fi
+
+if  [ "$state" = "full" ]; then
+    n_task=51
+fi
+
+
+
+
+
+
 cmd="python run_simulation.py --task_name income --test_frac 0.1 --scaler standard --optimizer sgd \
 --batch_size $batch_size  --learning_rate $learning_rate --device $device \
 --by_epoch --local_steps $local_epochs --num_rounds $num_rounds --seed $seed \
---data_dir ./data/income  --log_freq 5 --save_freq 1 \
---model_config_path ./../fedklearn/configs/income/$state/models/net_config.json --split_criterion correlation \
---n_tasks 10 --state $state --mixing_coefficient $heterogeneity \
---chkpts_dir ./income/$state/heter/$heterogeneity/10/32/1/ \
- --logs_dir ./logs/income/$state/heter/$heterogeneity/10/32/1/ \
- --metadata_dir ./metadata/income/$state/heter/$heterogeneity/10/32/1/"
+--data_dir ./data/$task_name  --log_freq 10 --save_freq 1 \
+--model_config_path ./../fedklearn/configs/$task_name/$state/models/net_config.json --split_criterion $split_criterion \
+ --state $state  $dirs"
+
+
+
 
  if [ "$force_generation" = "true" ]; then
     cmd="$cmd --force_generation"
