53a0cfb4b66a27104fbbbda77f6ba95ceb5288ff
fdiana <francesco.diana@inria.fr>
2025-05-07 18:56:46 +0200

    add medical cost table 3 bash scripts

---
+++ bash_scripts/medical_cost/privacy/run_aia_active_gb.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+cd ../../../scripts
+
+original_dir=$(pwd)
+
+if [ -z "$1" ]; then
+    seed=0
+else
+    seed=$1
+fi
+
+if [ -z "$2" ]; then
+    device='cuda'
+else
+    device=$2
+fi
+
+n_tasks=2
+n_local_steps=1
+learning_rates="100 1000 10000 100000 1000000"
+sensitive_attribute="smoker_yes"
+optimizer="sgd"
+attacked_round=99
+batch_size=32
+
+# privacy parameters (needed only for the paths)
+delta=1e-6
+clip=5e5
+epsilon=1
+
+metadata_dir="./metadata/seeds/$seed/privacy/$epsilon/$delta/$clip/medical_cost/$n_tasks/$batch_size/$n_local_steps/$optimizer"
+logs_dir="./logs/seeds/$seed/privacy/$epsilon/$delta/$clip/medical_cost/$n_tasks/$batch_size/$n_local_steps/$optimizer/active_${attacked_round}_gb_aia"
+results_path="./results/seeds/$seed/privacy/$epsilon/$delta/$clip/medical_cost/$n_tasks/$batch_size/$n_local_steps/$optimizer/active_${attacked_round}_gb_aia.json"
+
+#NOTE: if you change the number of local epochs, you need to adapt the keep_rounds_frac values
+keep_rounds_frac="0. 0.05 0.10 0.20 0.5 1.0"  # Add keep_rounds_frac values
+
+cmd="python run_gb_aia.py \
+--num_rounds 100 \
+--device $device \
+--seed $seed \
+--sensitive_attribute $sensitive_attribute \
+--sensitive_attribute_type binary \
+--keep_rounds_frac $keep_rounds_frac \
+--metadata_dir $metadata_dir \
+--logs_dir $logs_dir \
+--learning_rate $learning_rates \
+--results_path $results_path \
+--keep_first_rounds \
+--attacked_round $attacked_round \
+--active_server \
+--track_time"
+
+echo "Running $cmd"
+eval $cmd
+
+cd $original_dir 
---
+++ bash_scripts/medical_cost/privacy/run_aia_mb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd ../../../scripts
+
+original_dir=$(pwd)
+
+if [ -z $1 ]; then
+    device='cuda'
+else
+    device=$1
+fi
+
+if [ -z $2 ]; then
+    seed=42
+else
+    seed=$2
+fi
+
+if [ -z $3 ]; then
+    attacked_round=99
+else
+    attacked_round=$3
+fi
+
+batch_size=32
+n_local_steps=1
+optimizer="sgd"
+n_tasks=2
+active_rounds="0 9 49"
+
+# privacy parameters (needed only for the paths)
+delta=1e-6
+clip=5e5
+epsilon=1
+
+metadata_dir="./metadata/seeds/$seed/privacy/$epsilon/$delta/$clip/medical_cost/$n_tasks/$batch_size/$n_local_steps/$optimizer"
+results_dir="./results/seeds/$seed/privacy/$epsilon/$delta/$clip/medical_cost/$n_tasks/$batch_size/$n_local_steps/$optimizer"
+
+cmd="python run_mb_aia.py \
+--device $device \
+--seed $seed \
+--sensitive_attribute smoker_yes \
+--sensitive_attribute_type binary \
+--metadata_dir $metadata_dir \
+--results_dir $results_dir \
+--active_rounds $active_rounds \
+--attacked_round $attacked_round \
+--use_oracle " 
+
+echo $cmd
+eval $cmd
+
+cd $original_dir 
